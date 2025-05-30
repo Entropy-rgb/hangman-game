06e0cd7698ca99a4b9dc465ced7fd49546f64842
Entropy-rgb <som3shkamad@gmail.com>
2025-05-30 16:39:16 +0530

    initial commit

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 import random
 import nltk
+from nltk.corpus import words
 
+nltk.download('words')
+word_list = words.words()
+
+# adding a filter to the words to take words whose lenghts are from 5 to 9 letters
+filtered_words = [words.lower() for word in word_list if 5 <= len(word) <= 9 and word.isalpha()]
+
+randomWord = random.choice(filtered_words)
+lenghtOfWord = len(randomWord)
 
 HANGMAN_PICS = ['''
    +---+
@@ -39,3 +48,32 @@ HANGMAN_PICS = ['''
   / \  |
       ===''']
 
+# mistakes = 0
+# correct = 0
+
+# print('''
+# ==========================================
+
+#        WELCOME TO THE HANGMAN GAME 
+
+#               HAVE FUN :)
+
+# ==========================================
+# ''')
+
+# print(lenghtOfWord*'_ ')
+
+# guess = input('What\'s Your Guess : ')
+# guess = guess.lower()
+
+# for i in range(0,lenghtOfWord):
+#     if randomWord[i] == guess[i]:
+#         print(randomWord[i]+" ",end="")
+#         correct = correct + 1
+#     else:
+#         print('_ ',end="")
+
+# if correct == lenghtOfWord:
+#     print("You Win ! The correct Word is "+ randomWord)
+# else:
+#     pass
